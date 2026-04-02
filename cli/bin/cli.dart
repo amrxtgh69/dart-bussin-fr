0d1bc3ac57eeb7f210f14c220f0c6b7cd754b35c
amrxtgh <ghimireamrit619@gmail.com>
2026-04-03 01:02:32 +0545

    update 01

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -6,8 +6,11 @@ void main(List<String> arguments) {
     print("Hello Dario");
   } else if (arguments.first == "version") {
     print("Dartpedia CLI version $version");
-  } else if (arguments.first == "search") {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+  } else if (arguments.first == "search") { 
+    /* `arguments.sublist(1)` creates the list containing all elements
+     *  of argument after the first element (search)
+    */
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null; 
     searchWikipedia(inputArgs);
   } else {
     printUsage();
@@ -17,10 +20,13 @@ void main(List<String> arguments) {
 void searchWikipedia(List<String>? arguments) {
   final String articleTitle;
 
+  // if the user didn't pass any arguments then request an article title 
   if (arguments == null || arguments.isEmpty) {
     print("Please provide article title");
+    // await user input and provide a default empty '' string if the input is null
     articleTitle = stdin.readLineSync() ?? '';
   } else {
+    // join the list into the single string 
     articleTitle = arguments.join(' ');
   }
 
