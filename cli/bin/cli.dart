df1e69af353d86f6573ac08344e79ef42d8c78d5
amrxtgh <ghimireamrit619@gmail.com>
2026-04-05 00:24:09 +0545

    fix

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,7 +3,7 @@ import 'package:cli/cli.dart';
 import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments == null) {
+  if (arguments.isEmpty || arguments == "help") {
     printUsage();
   } else if (arguments.first == "version") {
     print("Dartpedia CLI version $version");
